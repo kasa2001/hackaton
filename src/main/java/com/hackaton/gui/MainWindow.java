25327e04c1519328f5ab2179ace04768331ec646
bolek <arek2159@gmail.com>
2017-10-16 19:16:31 +0200

    Główne okno i wybór pliku

--- src/main/java/com/hackaton/gui/MainWindow.java
+++ src/main/java/com/hackaton/gui/MainWindow.java
@@ -8,26 +8,39 @@ import java.io.File;
 
 public class MainWindow {
 
-    private MainWindow(){
+    private MainWindow() {
         JFrame f = new JFrame("Program");
         JPanel contentPane = new JPanel();
-        contentPane.setLayout(new FlowLayout());
+        JPanel top = new JPanel();
+        JPanel bottom = new JPanel();
 
+        contentPane.setLayout(new BorderLayout());
+        top.setLayout(new FlowLayout());
+        bottom.setLayout(new FlowLayout());
+
+
+        JLabel label = new JLabel("plik");
         JButton button = new JButton("Wybierz plik");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fc = new JFileChooser();
                 int val = fc.showOpenDialog(contentPane);
-                if(val == JFileChooser.APPROVE_OPTION){
+                if (val == JFileChooser.APPROVE_OPTION) {
                     File file = fc.getSelectedFile();
                     System.out.println(file.getAbsolutePath());
-                }else{
+                } else {
                     System.out.println("canceled");
                 }
             }
         });
-        contentPane.add(button);
+
+
+        top.add(label);
+        bottom.add(button);
+
+        contentPane.add(top, BorderLayout.NORTH);
+        contentPane.add(bottom, BorderLayout.SOUTH);
 
         f.setContentPane(contentPane);
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -35,7 +48,7 @@ public class MainWindow {
         f.setVisible(true);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
